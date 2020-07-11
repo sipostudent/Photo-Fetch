c61b8d641875bbb0e953797ae2e16b28012f93ff
Sipo Charles <sipocharles@yahoo.co.uk>
2020-07-11 02:32:25 +0100

    Refactored code

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,12 @@ form.addEventListener('submit', (e) => {
     searchPhotos(searchValue);
 });
 
-
 function updateInput(e) {
     searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
         method: 'GET',
         headers: {
             Accept: 'application/json',
@@ -25,38 +24,30 @@ async function curatedPhotos() {
         }
     });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePictures(data) {
     data.photos.forEach(photo => {
         console.log(photo)
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
         galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-        <p>${photo.photographer_url}</p>
-        `;
+            <p>${photo.photographer}</p>
+            <p>${photo.photographer_url}</p>
+            `;
         gallery.appendChild(galleryImg);
     });
 }
 
+async function curatedPhotos() {
+    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+    generatePictures(data);
+}
+
 async function searchPhotos(query) {
-    const dataFetch = await fetch(
-        `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page1`, {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json',
-            Authorization: auth
-        }
-    });
-    const data = await dataFetch.json();
-    data.photos.forEach(photo => {
-        console.log(photo)
-        const galleryImg = document.createElement('div');
-        galleryImg.classList.add('gallery-img');
-        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
-        <p>${photo.photographer}</p>
-        <p>${photo.photographer_url}</p>
-        `;
-        gallery.appendChild(galleryImg);
-    });
+    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page1`);
+    generatePictures(data);
 }
 
 curatedPhotos();
