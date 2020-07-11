55eede91bde2617e0f5ac4c25b492cb09c703b1a
Sipo Charles <sipocharles@yahoo.co.uk>
2020-07-11 01:27:15 +0100

    Fetching images

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const auth = "563492ad6f91700001000001f5dbd18e3c13456c8dcc3901e7aff665";
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('.submit-btn');
+let searchValue;
+
+
+async function curatedPhotos() {
+    const dataFetch = await fetch("https://api.pexels.com/v1/curated?per_page=15&page=1", {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+            Authorization: auth
+        }
+    });
+    const data = await dataFetch.json();
+    data.photos.forEach(photo => {
+        console.log(photo)
+        const galleryImg = document.createElement('div');
+        galleryImg.classList.add('gallery-img');
+        galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+        <p>${photo.photographer}</p>
+        <p>${photo.photographer_url}</p>
+        `;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+curatedPhotos();
