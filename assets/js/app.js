9729068d5d3d35d2c2db5e91ec21ef1ef7e308da
Sipo Charles <sipocharles@yahoo.co.uk>
2020-07-11 08:25:10 +0100

    Back to top button

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,19 @@ const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
 const form = document.querySelector('.search-form');
 let searchValue;
+const more =  document.querySelector(".more");
+let page = 1;
+let fetchLink;
+let currentSearch;
 
 // Event Listeners
 searchInput.addEventListener('input', updateInput);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    currentSearch = (searchValue);
     searchPhotos(searchValue);
 });
+more.addEventListener("click", loadMore);
 
 function updateInput(e) {
     searchValue = e.target.value;
@@ -29,13 +35,13 @@ async function fetchApi(url) {
 
 function generatePictures(data) {
     data.photos.forEach(photo => {
-        console.log(photo)
+        // console.log(photo)
         const galleryImg = document.createElement('div');
         galleryImg.classList.add('gallery-img');
         galleryImg.innerHTML = `
             <div class=gallery-info>
             <p>${photo.photographer}</p>
-            <a href=${photo.src.original}>Download</a>
+            <a href=${photo.src.original} target="_blank" rel="noopener noreferrer">Download</a>
             </div>
             <img src=${photo.src.large}></img>
             `;
@@ -44,13 +50,15 @@ function generatePictures(data) {
 }
 
 async function curatedPhotos() {
-    const data = await fetchApi("https://api.pexels.com/v1/curated?per_page=15&page=1");
+  fetchLink = "https://api.pexels.com/v1/curated?per_page=15&page=1";
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
 async function searchPhotos(query) {
     clear();
-    const data = await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page1`);
+    fetchLink = `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page1`;
+    const data = await fetchApi(fetchLink);
     generatePictures(data);
 }
 
@@ -59,4 +67,24 @@ function clear() {
     searchInput.value = "";
 }
 
+async function loadMore() {
+  page++;
+  if (currentSearch) {
+    fetchLink = `https://api.pexels.com/v1/search?query=${currentSearch}+query&per_page=15&page${page}`;
+  } else {
+    fetchLink = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+  }
+  const data = await fetchApi(fetchLink)
+  generatePictures(data);
+}
+
 curatedPhotos();
+
+// Float Back To Top Smooth Scrolling
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('#scroll').fadeIn();
+  } else {
+    $('#scroll').fadeOut();
+  }
+});
